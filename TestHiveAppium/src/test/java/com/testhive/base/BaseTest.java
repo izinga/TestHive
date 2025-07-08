b9333a6a05384e3c9f4fcaa08863349186513b9b
Om Narayan <omnarayan@robustest.com>
2025-07-08 11:11:50 +0530

    added abs path

--- TestHiveAppium/src/test/java/com/testhive/base/BaseTest.java
+++ TestHiveAppium/src/test/java/com/testhive/base/BaseTest.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import io.appium.java_client.android.options.UiAutomator2Options;
 import java.net.MalformedURLException;
+
 import java.net.URL;
 import java.time.Duration;
 
@@ -18,11 +20,11 @@ public class BaseTest {
         // --- Desired Capabilities Setup ---
         DesiredCapabilities caps = new DesiredCapabilities();
 
-        caps.setCapability("platformName", "Android");
-        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("appium:platformName", "Android");
+        caps.setCapability("appium:automationName", "UiAutomator2");
 
-        caps.setCapability("appPackage", "com.testhiveapp");
-        caps.setCapability("appActivity", ".MainActivity");
+        caps.setCapability("appium:appPackage", "com.testhiveapp");
+        caps.setCapability("appium:appActivity", ".MainActivity");
 
         // --- IMPORTANT ---
         // You must build the TestHiveApp and install the APK on your device/emulator.
@@ -33,7 +35,12 @@ public class BaseTest {
         // URL appiumServerUrl = new URL("http://0.0.0.0:4723");
 
         // --- Driver Initialization ---
-        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
+        UiAutomator2Options options = new UiAutomator2Options()
+            .setAppPackage("com.testhiveapp")
+            .setAppActivity(".MainActivity");
+
+        URL appiumServerUrl = new URL("http://0.0.0.0:4723");
+        driver = new AndroidDriver( appiumServerUrl, options);
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
